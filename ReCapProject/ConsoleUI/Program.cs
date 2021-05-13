2572fd28fbf4a3f4a41e27dc20a58b2148e447c3
BilalOzel7 <78275900+BilalOzel7@users.noreply.github.com>
2021-05-13 23:41:30 +0300

    ReCapProject-8-gun-odev
    ReCapProject 8 gun Odev

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemoryCarDal;
+using System;
+
+namespace ConsoleUI
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+
+        }
+    }
+}
+ 
---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public class ReCapProjectContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-V3FGGNT\SQLEXPRESS;Database=ReCapProject;Trusted_Connection=true");
+        }
+
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Brand> Brand { get; set; }
+        public DbSet<Color> Color { get; set; }
+    }
+}
